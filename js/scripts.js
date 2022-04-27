d0e133016fb37ad33d75909e20254c617976024a
Yvonne <yveswangui@gmail.com>
2022-04-27 23:52:52 +0300

    add input sections

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("form#slide").submit(function(e){
+        e.preventDefault();
+    });
+});
+let username = document.getElementById('user-name')
+console.log(username)
